8706898c8581b7c445761040710761e4740b24fc
bakajinsei <cqggff@gmail.com>
2017-03-31 15:24:16 +0800

    Change online config

--- config/config-online.js
+++ config/config-online.js
@@ -4,8 +4,7 @@ module.exports = {
         type: 'dateFileLog',
         level: 'info'
     },
-    downloadDomain:'http://127.0.0.1:8001'
-    // clientDomain:'https://www.acgo.club',
-    // clientPath:'https://www.acgo.club/client',
-    // downloadDomain:'https://oak0s7wv0.qnssl.com'
+    clientDomain:'https://www.acgo.club',
+    clientPath:'https://www.acgo.club/client',
+    downloadDomain:'https://oak0s7wv0.qnssl.com'
 };
